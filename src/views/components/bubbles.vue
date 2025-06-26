67e3755c773a83e1751f2ebb1de1023c49ce5145
yuji <2389975296@qq.com>
2025-06-26 14:21:37 +0800

    fix

--- src/views/components/bubbles.vue
+++ src/views/components/bubbles.vue
@@ -6,12 +6,13 @@
             background: ball.color,
             left: `${ball.x}px`,
             top: `${ball.y}px`
-        }"></div>
+        }">
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref} from 'vue';
 
 const balls = ref([]);
 
@@ -27,9 +28,6 @@ function createBall() {
         originY,
         x: originX,
         y: originY,
-        angle: Math.random() * 9 * Math.PI, // 随机初始角度
-        // maxOffset: 15, // 最大偏移量
-        // speed: 0.1 // 浮动速度
     };
 }
 
@@ -38,39 +36,6 @@ for (let i = 0; i < 11; i++) {
     balls.value.push(createBall());
 }
 
-// // 动画更新
-// let animationFrameId = null;
-// function update() {
-//     balls.value.forEach(ball => {
-//         // ball.angle += ball.speed;
-//         ball.x = ball.originX + Math.sin(ball.angle) * ball.maxOffset;
-//         ball.y = ball.originY + Math.cos(ball.angle) * ball.maxOffset;
-//     });
-//     animationFrameId = requestAnimationFrame(update);
-// }
-
-// // 启动动画
-// onMounted(() => {
-//     animationFrameId = requestAnimationFrame(update);
-// });
-
-//清理动画
-// onUnmounted(() => {
-//     if (animationFrameId) {
-//         cancelAnimationFrame(animationFrameId);
-//     }
-// });
-
-// 窗口大小变化时调整小球位置
-// window.addEventListener('resize', () => {
-//     balls.value.forEach(ball => {
-//         const size = ball.size;
-//         ball.originX = Math.min(ball.originX, window.innerWidth - size);
-//         ball.originY = Math.min(ball.originY, window.innerHeight - size);
-//         ball.x = ball.originX + Math.sin(ball.angle) * ball.maxOffset;
-//         ball.y = ball.originY + Math.cos(ball.angle) * ball.maxOffset;
-//     });
-// });
 </script>
 
 <style scoped>
