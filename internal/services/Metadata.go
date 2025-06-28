c2945ade06dff75019489545da27f3cfb181a379
Ruixuan Lin <1356088661@qq.com>
2025-06-28 15:05:39 +0800

    新增获取MetaData详情数据接口

--- internal/services/Metadata.go
+++ internal/services/Metadata.go
@@ -2,9 +2,13 @@ package services
 
 import (
 	"GeneReport_platform/api/dto"
+	"GeneReport_platform/configs"
 	"GeneReport_platform/internal/dao"
 	"GeneReport_platform/pkg/appErrors"
+	"encoding/json"
 	"github.com/mitchellh/mapstructure"
+	"net/http"
+	"reflect"
 )
 
 var (
@@ -54,10 +58,50 @@ func (m *MetadataService) GetMetadataDetailByDataHash(dataHash string) (map[stri
 		return nil, appErrors.New(503, "获取Metadata详细信息失败", err)
 	}
 
-	results, err := GetDataImpl(metadata.ProfileID, metadata.Category)
+	results, err := m.GetDataImpl(metadata.ProfileID, metadata.Category)
 	if err != nil {
 		return nil, err
 	}
 
 	return results, nil
+}
+
+// GetDataImpl 获取GeneType数据
+func (m *MetadataService) GetDataImpl(profileId, category string) (map[string]interface{}, error) {
+	unique := dto.UniqueProfiles{}
+	//在数据库查出profileId一样记录
+	if profileId != "" {
+		configs.DB.Where("profile_id = ?", profileId).Find(&unique)
+	}
+	if unique.Status == 0 {
+		return nil, appErrors.New(http.StatusOK, "数据还没处理完成，请稍等!")
+	}
+
+	// 获取结构体类型
+	t, ok := dto.GetStructType(category)
+	if !ok {
+		return nil, appErrors.New(http.StatusBadRequest, "无效的 category", nil)
+	}
+
+	// 创建切片用于查询结果
+	sliceType := reflect.SliceOf(t)
+	results := reflect.MakeSlice(sliceType, 0, 0)
+
+	// GORM 查询
+	err := configs.DB.Where("profile_id = ?", profileId).Find(&results).Error
+	if err != nil {
+		return nil, appErrors.New(http.StatusInternalServerError, "查询失败", err)
+	}
+
+	// 转为 map[string]interface{}
+	data, err := json.Marshal(results.Interface())
+	if err != nil {
+		return nil, appErrors.New(http.StatusInternalServerError, "序列化失败", err)
+	}
+	var resultMap map[string]interface{}
+	if err := json.Unmarshal(data, &resultMap); err != nil {
+		return nil, appErrors.New(http.StatusInternalServerError, "反序列化失败", err)
+	}
+
+	return resultMap, nil
 }
