9710249a044adc5c4b415c951d502ea1204b1035
xzyJJboom <1356088661@qq.com>
2025-07-04 22:35:02 +0800

    debug

--- internal/contracts/sharingPlatformContract/forInvoking.go
+++ internal/contracts/sharingPlatformContract/forInvoking.go
@@ -47,10 +47,10 @@ func init() {
 	AdminPrivateKeyHex = "9f5eb599dd2ff51f67724a793a6d702bcc273b3afe3e3bbc0e2870ed11594432"
 
 	//初始化已部署的Metadata合约地址
-	MetaDataContractAddress = "0x94Dc5d38BCC843B6b8d0B973e12e28271060453F"
+	MetaDataContractAddress = "0xE4386cf966aC0Ca0757020d6F0C07AB3dCE13a2D"
 
 	// 初始化已部署的sharingPlatform_v3合约地址
-	PlatformContractAddressHex = "0x540699c76fD4C5939426D0F7fe09e9b07907f0e5"
+	PlatformContractAddressHex = "0x9FF72c7E4a7c0d3126632C05e07028e1aa3004d3"
 
 	// 解码私钥
 	privateKeyBytes, err := hex.DecodeString(AdminPrivateKeyHex)
