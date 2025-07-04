5b21384834a2e3eae1abc99d20d5046d683bd36b
Ruixuan Lin <1356088661@qq.com>
2025-07-04 17:08:42 +0800

    debug 合约新增测试的相关功能

--- internal/contracts/.sol/sharingPlatform_v3.sol
+++ internal/contracts/.sol/sharingPlatform_v3.sol
@@ -162,5 +162,8 @@ contract SharingPlatform is ERC20 {
         return metadata.verifyViewAccess(dataHash, msg.sender);
     }
 
-
+    //测试GeneSharing是否存在
+    function TestIsGeneSharingExist(address _geneSharingAddress)external view returns (bool) {
+        return _geneSharingContract[_geneSharingAddress];
+    }
 }
