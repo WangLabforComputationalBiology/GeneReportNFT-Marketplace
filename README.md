f52f1b3eb3edda68ede1e75195e4a0342d2bb5ae
Yuji <2389975296@qq.com>
2025-07-16 14:00:52 +0800

    文档编写

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
 # Vue3 Webpage for Biochainer
-Technology Stack: Vue3 + Ethers.js + Pinia + Element Plus + Vite + Axios + Vue Router 
-Author: Tang Jiaming(SZTU BDI) 
-Date: 2024.12 - 2025.8
 
+Author: Tang Jiaming@SZTU BDI
+Date: 2025.2 - 2025.8
+
+## 项目介绍
+
+基于 Vue3 的 BioChainer 前端页面，提供账户验证、合约交互功能以及数据共享功能模块。合约通过 MetaMask 钱包交互。
+
+
+## **目录**
+- [项目主要技术栈](#项目主要技术栈)
+- [项目启动](#项目启动)
+- [项目部署](#项目部署)
+- [部署说明](#部署说明)
+
+
+## 项目主要技术栈
+
+- Vue3
+- Vite
+- Vue Router
+- Pinia
+- Element Plus
+- Axios
+- Ethers.js
+
+## 项目启动
+
+```bash
+# 安装依赖
+npm install
+
+# 运行项目
+npm run dev
+```
+
+## 项目部署
+
+```bash
+# 构建项目
+npm run build
+
+```
+##  部署说明
--- src/axios/aixos.js
+++ src/axios/aixos.js
@@ -1,22 +1,21 @@
 import axios from 'axios';
 import { useWalletStore } from '@/stores/account';
-const baseURL = import.meta.env.VITE_APP_BASE_URL;
-//VITE_APP_BASE_URL=http://10.108.10.51:7070
+const baseURL = import.meta.env.VITE_APP_BASE_URL;  //VITE_APP_BASE_URL=http://10.108.10.51:7070
 
 const Api = axios.create({
     baseURL: baseURL,
     headers: {
         // 'Authorization': useWalletStore().token, //axios.create只会进行一次初始化，导致更换账户时token没有更新
-        // 'Content-Type': 'application/json'
     }
 });
 
-/**添加请求拦截器，动态更新 token
- */
+/**添加请求拦截器，动态更新 token */
 Api.interceptors.request.use((config) => {
     const walletStore = useWalletStore();
     if (walletStore.token) {
         config.headers.Authorization = walletStore.token; // 每次请求前重新设置
+    }else{
+        console.log('No token.');
     }
     return config;
 });
